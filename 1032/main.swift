8e71ea1e3078fb119288cc393113a50361c28493
luninj <103933747+luninj@users.noreply.github.com>
2022-04-23 05:35:23 +0900

    블로깅 전용 그 두번째

--- 1032/main.swift
+++ 1032/main.swift
@@ -1,15 +1,20 @@
-let x = Int(readLine()!)!
-var y = Array(readLine()!)
-
-for i in 1..<x {
-    var z = Array(readLine()!)
-    for j in 0..<y.count {
-        if y[j] != z[j] {
-            y[j] = "?"
+func Logic(_ y : [Character]) -> String {
+    var v = y
+    for i in 1..<x {
+        var z = Array(readLine()!)
+        for j in 0..<y.count {
+            if y[j] != z[j] {
+                v[j] = "?"
+            }
         }
     }
+    
+    return v.map{String($0)}.joined()
 }
 
-print(y.map{String($0)}.joined())
+let x = Int(readLine()!)!
+var y = Array(readLine()!)
+print(Logic(y))
+
 
 //정리할 것 map함수, joined() , enumerated(), Array()
